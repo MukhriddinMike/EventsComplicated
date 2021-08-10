e780902ea1a9e4c333e35913828f78f3b4def563
MukhriddinMike <miwady@mail.ru>
2021-08-11 01:28:01 +0900

    Showcase

---
+++ components/Showcase.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Showcase = () => {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to the party</h1>
+      <h2>Find the hottest Dj events</h2>
+    </div>
+  )
+}
+
+export default Showcase
